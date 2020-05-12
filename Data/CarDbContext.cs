074175e0cb2e9f38611d250a93af20637e64a800
Ahmad Hammouda <ahmedvb@hotmail.com>
2020-05-12 21:20:19 +0200

    core202005122120

--- Data/CarDbContext.cs
+++ Data/CarDbContext.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using my_core.Models;
 
 namespace my_core.Data
 {
     public class CarDbContext:DbContext
     {
-        
+        public CarDbContext(DbContextOptions<CarDbContext>opt):base(opt)
+        {
+            
+        }
+        public DbSet<Car> cars { get; set; }
     }
 }
